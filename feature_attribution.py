bd4a24da425d418bf39a24e715bcf0bf146d437e
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-09-11 17:23:11 +0200

    Simplified feature attribution algorithm

--- feature_attribution.py
+++ feature_attribution.py
@@ -1,8 +1,3 @@
 def calculate_very_basic_feature_attribution(anomaly: int, anomaly_data: dict):
     anomaly_index = anomaly_data["anomalies"][anomaly]["index"]
-    anomaly_amount = len(anomaly_data["sensors"])
-    output_data = []
-    for i in range(anomaly_amount):
-        calc_data = anomaly_data["deep-error"][i][anomaly_index] / anomaly_data["error"][anomaly_index] * 100
-        output_data.append(calc_data)
-    return output_data
+    return [anomaly_data["deep-error"][i][anomaly_index] / anomaly_data["error"][anomaly_index] * 100 for i in range(len(anomaly_data["sensors"]))]
