8dd6fa6c3a715d8fbf6592c3597a3caac74a58e5
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-09-11 17:17:52 +0200

    Fixed dictionary index values in Explainability

--- feature_attribution.py
+++ feature_attribution.py
@@ -1,3 +1,8 @@
 def calculate_very_basic_feature_attribution(anomaly: int, anomaly_data: dict):
     anomaly_index = anomaly_data["anomalies"][anomaly]["index"]
-    return [anomaly_data["error"][i][anomaly_index] / anomaly_data["output_error"][anomaly_index] * 100 for i in range(len(anomaly_data["sensors"]))]
+    anomaly_amount = len(anomaly_data["sensors"])
+    output_data = []
+    for i in range(anomaly_amount):
+        calc_data = anomaly_data["deep-error"][i][anomaly_index] / anomaly_data["error"][anomaly_index] * 100
+        output_data.append(calc_data)
+    return output_data
